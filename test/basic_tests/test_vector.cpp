0d493c269540aea1af62d80a324547671a196d32
KetchuppOfficial <dolgov04@list.ru>
2022-10-06 15:19:44 +0300

    Tests for collinearity, orthogonality and coplanarity added; the test for triple product extended

--- test/basic_tests/test_vector.cpp
+++ test/basic_tests/test_vector.cpp
@@ -211,6 +211,62 @@ TEST (Vectors, Triple_Product)
     Vector vec_5 {-937491.419, -0.0000007264, 0.00011214};
     Vector vec_6 {64783191.971, -0.000021412, 3.1415926535};
     EXPECT_TRUE (cmp::are_equal (triple_product (vec_4, vec_5, vec_6), 84827167.6));
+
+    double k_1 {7149.870};
+    double k_2 {-0.0000000327561};
+    Vector vec_7 {1234.5, 6789.0, -38732.54};
+    Vector vec_8 {2874.12, -12648.9871, 0.0000067821};
+    Vector vec_9 {k_1 * vec_7.x_ + k_2 * vec_8.x_,
+                  k_1 * vec_7.y_ + k_2 * vec_8.y_,
+                  k_1 * vec_7.z_ + k_2 * vec_8.z_};
+    EXPECT_TRUE (cmp::are_equal (triple_product (vec_7, vec_8, vec_9), 0.0));
+}
+
+TEST (Vectors, Are_Collinear)
+{
+    Vector vec_1 {32802.0, 81749.8721, -0.00000065271210841};
+    Vector null {};
+    EXPECT_TRUE (are_collinear (vec_1, 172684.187239 * vec_1));
+    EXPECT_TRUE (are_collinear (vec_1, 0.00000000038742 * vec_1));
+    EXPECT_TRUE (are_collinear (vec_1, null));
+
+    Vector vec_2 {-897461.56547175, 7.761208, -0.000000000000172564890};
+    Vector vec_3 {vec_2.x_, vec_2.y_, vec_2.z_ + 1};
+    EXPECT_FALSE (are_collinear (vec_2, vec_3));
+}
+
+TEST (Vectors, Are_Orthogonal)
+{
+    Vector vec_1 {0.0002467197, 6570082.97240, -0.37813};
+    Vector vec_2 {1 / vec_1.x_, 1 / vec_1.y_, -2 / vec_1.z_};
+    Vector null {};
+    EXPECT_TRUE (are_orthogonal (vec_1, vec_2));
+    EXPECT_TRUE (are_orthogonal (vec_1, null));
+
+    Vector vec_3 {8279.249, 1.0074197, -718.24678};
+    Vector vec_4 {0.0000274814, 0.019238764, -0.279};
+    EXPECT_FALSE (are_orthogonal (vec_3, vec_4));
+}
+
+TEST (Vectors, Are_Coplanar)
+{
+    double k_1 {6736.8};
+    double k_2 {-0.32};
+    Vector vec_1 {78364.03174581, -0.018264, 46247124152.78};
+    Vector vec_2 {6742.761, 0.1, -267545.53};
+    Vector vec_3 {k_1 * vec_1.x_ + k_2 * vec_2.x_,
+                  k_1 * vec_1.y_ + k_2 * vec_2.y_,
+                  k_1 * vec_1.z_ + k_2 * vec_2.z_};
+    Vector null {};
+    EXPECT_TRUE (are_coplanar (vec_1, vec_2, vec_3));
+    EXPECT_TRUE (are_coplanar (vec_1, vec_2, null));
+    EXPECT_TRUE (are_coplanar (vec_1, null, vec_3));
+    EXPECT_TRUE (are_coplanar (null, vec_2, vec_3));
+
+    Vector e_1 {1.0, 0.0, 0.0};
+    Vector e_2 {0.0, 1.0, 0.0};
+    Vector e_3 {0.0, 0.0, 1.0};
+    EXPECT_FALSE (are_coplanar (e_1, e_2, e_3));  
 }
 
 TEST (Vectors, Ctor_From_Points)
