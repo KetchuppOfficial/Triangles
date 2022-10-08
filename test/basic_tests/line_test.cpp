135400415b9924696b7b0cdedab3bf49e40951e7
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-10-08 04:28:03 +0300

    wrote test for distance()(btw lines)

--- test/basic_tests/line_test.cpp
+++ test/basic_tests/line_test.cpp
@@ -6,11 +6,11 @@ using namespace Geom_Objects;
 
 TEST(Lines, operator_equal)
 {
-    Point pt1 {1, 1, 1};
-    Point pt2 {0, 0, 0};
+    Point pt1 {1.0, 1.0, 1.0};
+    Point pt2 {0.0, 0.0, 0.0};
 
-    Vector vec1 {1, 1, 1};
-    Vector vec2 {33, 78, 96};
+    Vector vec1 {1.0, 1.0, 1.0};
+    Vector vec2 {33.1, 78.4, 96.7};
 
     Line line1 {pt1, pt2};
     Line line2 {pt1, vec1};
@@ -27,11 +27,11 @@ TEST(Lines, operator_equal)
 
 TEST(Lines, _are_parallel)
 {
-    Point pt1 {0, 0, 0};
-    Point pt2 {0, 0, 1};
+    Point pt1 {0.0, 0.0, 0.0};
+    Point pt2 {0.0, 0.0, 1.0};
 
-    Vector vec1 {1, 2, 3};
-    Vector vec2 {37, 767, -39};
+    Vector vec1 {1.0, 2.0, 3.0};
+    Vector vec2 {37.8, 767.6, -39.7};
 
     Line line1 {pt1, vec1};
     Line line2 {pt2, vec1};
@@ -53,17 +53,43 @@ TEST(Lines, _are_parallel)
 
 TEST(Lines, distance_btw_line_n_pt)
 {
-    Point pt1 {0, 0, 0};
-    Point pt2 {1, 0, 0};
-    Point pt3 {0, 1, 0};
+    Point pt1 {0.0, 0.0, 0.0};
+    Point pt2 {1.0, 0.0, 0.0};
+    Point pt3 {0.0, 1.0, 0.0};
 
-    Vector vec1 {0, 0, 1};
+    Vector vec1 {0.0, 0.0, 1.0};
 
     Line line {pt1, vec1};
 
-    EXPECT_TRUE(cmp::are_equal(distance(line, pt1), 0));
-    EXPECT_TRUE(cmp::are_equal(distance(line, pt2), 1));
-    EXPECT_TRUE(cmp::are_equal(distance(line, pt3), 1));
+    EXPECT_TRUE(cmp::are_equal(distance(line, pt1), 0.0));
+    EXPECT_TRUE(cmp::are_equal(distance(line, pt2), 1.0));
+    EXPECT_TRUE(cmp::are_equal(distance(line, pt3), 1.0));
 }
 
+TEST(Lines, distance_btw_lines)
+{
+    Point pt1 {0.0, 0.0, 0.0};
+    Point pt2 {1.0, 1.0, 1.0};
+    Point pt3 {1.0, 0.0, 0.0};
 
+    Vector vec1 {0.0, 0.0, 1.0};
+    Vector vec2 {1.0, 0.0, 1.0};
+    Vector vec3 {1.0, -1.0, 0.0};
+
+    Line line1 {pt1, vec1};
+    Line line2 {pt2, vec2};
+    EXPECT_TRUE(cmp::are_equal(distance(line1, line2), std::sqrt(2.0)));
+
+    Line line3 {pt3, vec1};
+    EXPECT_TRUE(cmp::are_equal(distance(line1, line3), 1.0));
+
+    Line line4 {pt1, vec2};
+    Line line5 {pt3, vec2};
+    EXPECT_TRUE(cmp::are_equal(distance(line4, line5), std::sqrt(2.0)));
+    
+    Line line6 {pt2, vec3};
+    EXPECT_TRUE(cmp::are_equal(distance(line1, line6), std::sqrt(2.0)));
+
+    EXPECT_TRUE(cmp::are_equal(distance(line1, line4), 0.0));
+    EXPECT_TRUE(cmp::are_equal(distance(line2, line6), 0.0));
+}
