26d1b4dc0152e11cf14a9b21c4d5d65f1b1ff213
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-10-08 03:46:01 +0300

    wrote test for are_parallel()

--- test/basic_tests/line_test.cpp
+++ test/basic_tests/line_test.cpp
@@ -3,7 +3,7 @@
 
 using namespace Geom_Objects;
 
-TEST(Lines, _operator==_)
+TEST(Lines, operator_equal)
 {
     Point pt1 {1, 1, 1};
     Point pt2 {0, 0, 0};
@@ -22,4 +22,30 @@ TEST(Lines, _operator==_)
     Line line5 {pt2, vec1};
     Line line6 {pt2, vec2};
     EXPECT_TRUE(line5 != line6);
+}
+
+TEST(Lines, _are_parallel)
+{
+    Point pt1 {0, 0, 0};
+    Point pt2 {0, 0, 1};
+
+    Vector vec1 {1, 2, 3};
+    Vector vec2 {37, 767, -39};
+
+    Line line1 {pt1, vec1};
+    Line line2 {pt2, vec1};
+    EXPECT_TRUE(are_parallel(line1, line2));
+
+    EXPECT_TRUE(are_parallel(line1, line1)); //basic logic
+
+    Line line3 {pt1, vec1};
+    Line line4 {pt1, vec2};
+    EXPECT_FALSE(are_parallel(line3, line4));
+
+    Line line5 {pt1,  vec1};
+    Line line6 {pt2, -vec1};
+    EXPECT_TRUE(are_parallel(line5, line6));
+
+    Line line7 {pt2, vec2};
+    EXPECT_FALSE(are_parallel(line3, line7));
 }
