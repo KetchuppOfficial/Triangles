0f6d35367c836d8e96bb62a3c42285fc59f7e02a
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-10-08 05:58:11 +0300

    fix line.hpp file after cringing clangformat

--- include/line.hpp
+++ include/line.hpp
@@ -9,53 +9,46 @@ namespace Geom_Objects {
 class Line
 {
     Vector drc_vec_;
-
 public:
-
     Point point_;
-    Line (const Point &p1, const Point &p2)
-        : point_ {p1}, drc_vec_ {p1.x_ - p2.x_, p1.y_ - p2.y_, p1.z_ - p2.z_}
+
+    Line(const Point& p1, const Point& p2): point_ {p1}, drc_vec_ {p1.x_ - p2.x_, p1.y_ - p2.y_, p1.z_ - p2.z_}
     {
-#ifndef RELEASE
+        #ifndef RELEASE
         if (p1 == p2)
             throw std::invalid_argument {"In constructor of Line(Point, Point): two equal points"};
-#endif
+        #endif
     }
 
-        const Vector& drc_vec(const Vector& new_vec)
-        {
-            #ifndef RELEASE
-            if (new_vec.is_zero())
-                throw std::invalid_argument{"In method of change drc_vec(): null vector"};
-            #endif
-            drc_vec_ = new_vec;
-            return drc_vec_;
-        }
-        bool operator== (const Line& other) const
-        {
-            if (point_ == other.point_)
-                return are_collinear(drc_vec_, other.drc_vec_);
-            return are_collinear(Vector{point_, other.point_}, drc_vec_);
-        }
-
-    const Vector &drc_vec (const Vector &new_vec)
+    Line(const Point& p, const Vector& vec): point_{p}, drc_vec_{vec}
     {
-#ifndef RELEASE
-        if (new_vec.is_zero ())
+        #ifndef RELEASE
+        if (vec.is_zero())
+            throw std::invalid_argument {"In constructor of Line(Point, Vector): null drc vector"};
+        #endif
+    }
+
+    const Vector& drc_vec() const {return drc_vec_;}
+
+    const Vector& drc_vec(const Vector &new_vec)
+    {
+        #ifndef RELEASE
+        if (new_vec.is_zero())
             throw std::invalid_argument {"In method of change drc_vec(): null vector"};
-#endif
+        #endif
+
         drc_vec_ = new_vec;
         return drc_vec_;
     }
 
-    bool operator== (const Line &other) const
+    bool operator==(const Line &other) const
     {
         if (point_ == other.point_)
-            return are_collinear (drc_vec_, other.drc_vec_);
-        return are_collinear (Vector {point_, other.point_}, drc_vec_);
+            return are_collinear(drc_vec_, other.drc_vec_);
+        return are_collinear(Vector {point_, other.point_}, drc_vec_);
     }
 
-    bool operator!= (const Line &other) const { return !(*this == other); }
+    bool operator!=(const Line &other) const { return !(*this == other); }
 };
 
 inline bool are_parallel(const Line& line1, const Line& line2) {return are_collinear(line1.drc_vec(), line2.drc_vec());}
@@ -63,22 +56,22 @@ inline bool are_parallel(const Line& line1, const Line& line2) {return are_colli
 inline double distance(const Line& line, const Point& pt)
 {
     Vector dot_vec {pt, line.point_};
-    double coef = scalar_product (dot_vec, line.drc_vec ()) /
-                  scalar_product (line.drc_vec (), line.drc_vec ());
-    return (dot_vec - coef * line.drc_vec ()).module ();
+    double coef = scalar_product(dot_vec, line.drc_vec()) /
+                  scalar_product(line.drc_vec(), line.drc_vec());
+    return (dot_vec - coef * line.drc_vec()).module();
 }
 
 inline double distance(const Point& pt, const Line& line) {return distance(line, pt);}
 
 inline double distance(const Line& line1, const Line& line2)
 {
-    if (are_parallel (line1, line2))
-        return vector_product ({line1.point_, line2.point_}, line1.drc_vec ()).module () /
-               line1.drc_vec ().module ();
+    if (are_parallel(line1, line2))
+        return vector_product({line1.point_, line2.point_}, line1.drc_vec()).module() /
+               line1.drc_vec().module();
 
     Vector dots_vec {line1.point_, line2.point_};
-    return triple_product (dots_vec, line1.drc_vec (), line2.drc_vec ().module ()) /
-           vector_product (line1.drc_vec (), line2.drc_vec ()).module ();
+    return triple_product(dots_vec, line1.drc_vec(), line2.drc_vec().module()) /
+           vector_product(line1.drc_vec(), line2.drc_vec()).module();
 }
 
 inline bool are_intersect(const Line& line1, const Line& line2) {return cmp::are_equal(distance(line1, line2), 0);}
@@ -88,21 +81,20 @@ inline bool is_belong(const Line& line, const Point& pt) {return is_belong(pt, l
 
 inline bool in_plane(const Line& line1, const Line& line2)
 {
-    return Comparison::are_equal (distance (line1, line2), 0);
+    return cmp::are_equal(distance(line1, line2), 0);
 }
 
-bool is_belong (const Point &pt, const Line &line)
+bool is_belong(const Point &pt, const Line &line)
 {
-    return Comparison::are_equal (distance (pt, line), 0);
+    return cmp::are_equal(distance(pt, line), 0);
 }
-bool is_belong (const Line &line, const Point &pt) { return is_belong (pt, line); }
+bool is_belong(const Line &line, const Point &pt) {return is_belong(pt, line);}
 
-bool in_plane (const Line &line1, const Line &line2)
+bool in_plane(const Line &line1, const Line &line2)
 {
-    if (are_parallel (line1, line2))
+    if (are_parallel(line1, line2))
         return true;
-    return are_intersect (line1, line2);
+    return are_intersect(line1, line2);
 }
-bool is_belong (const Line &line, const Point &pt) { return is_belong (pt, line); }
 
-}
+} //namespace Geom_Objects
