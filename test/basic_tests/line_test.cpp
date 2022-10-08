39ecc7ddc436bd5a7d4d54ecb737ba9e5fe03bc2
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-10-08 07:33:38 +0300

    use clang-format for files

--- test/basic_tests/line_test.cpp
+++ test/basic_tests/line_test.cpp
@@ -1,10 +1,11 @@
-#include <gtest/gtest.h>
-#include "line.hpp"
 #include "double_comparison.hpp"
+#include "line.hpp"
+
+#include <gtest/gtest.h>
 
 using namespace Geom_Objects;
 
-TEST(Lines, operator_equal)
+TEST (Lines, operator_equal)
 {
     Point pt1 {1.0, 1.0, 1.0};
     Point pt2 {0.0, 0.0, 0.0};
@@ -14,18 +15,18 @@ TEST(Lines, operator_equal)
 
     Line line1 {pt1, pt2};
     Line line2 {pt1, vec1};
-    EXPECT_TRUE(line1 == line2);
+    EXPECT_TRUE (line1 == line2);
 
-    Line line3 {pt1,  vec1};
+    Line line3 {pt1, vec1};
     Line line4 {pt1, -vec1};
-    EXPECT_TRUE(line3 == line4);
+    EXPECT_TRUE (line3 == line4);
 
     Line line5 {pt2, vec1};
     Line line6 {pt2, vec2};
-    EXPECT_TRUE(line5 != line6);
+    EXPECT_TRUE (line5 != line6);
 }
 
-TEST(Lines, _are_parallel)
+TEST (Lines, _are_parallel)
 {
     Point pt1 {0.0, 0.0, 0.0};
     Point pt2 {0.0, 0.0, 1.0};
@@ -35,23 +36,23 @@ TEST(Lines, _are_parallel)
 
     Line line1 {pt1, vec1};
     Line line2 {pt2, vec1};
-    EXPECT_TRUE(are_parallel(line1, line2));
+    EXPECT_TRUE (are_parallel (line1, line2));
 
-    EXPECT_TRUE(are_parallel(line1, line1)); //basic logic
+    EXPECT_TRUE (are_parallel (line1, line1)); //basic logic
 
     Line line3 {pt1, vec1};
     Line line4 {pt1, vec2};
-    EXPECT_FALSE(are_parallel(line3, line4));
+    EXPECT_FALSE (are_parallel (line3, line4));
 
-    Line line5 {pt1,  vec1};
+    Line line5 {pt1, vec1};
     Line line6 {pt2, -vec1};
-    EXPECT_TRUE(are_parallel(line5, line6));
+    EXPECT_TRUE (are_parallel (line5, line6));
 
     Line line7 {pt2, vec2};
-    EXPECT_FALSE(are_parallel(line3, line7));
+    EXPECT_FALSE (are_parallel (line3, line7));
 }
 
-TEST(Lines, distance_btw_line_n_pt)
+TEST (Lines, distance_btw_line_n_pt)
 {
     Point pt1 {0.0, 0.0, 0.0};
     Point pt2 {1.0, 0.0, 0.0};
@@ -61,12 +62,12 @@ TEST(Lines, distance_btw_line_n_pt)
 
     Line line {pt1, vec1};
 
-    EXPECT_TRUE(cmp::are_equal(distance(line, pt1), 0.0));
-    EXPECT_TRUE(cmp::are_equal(distance(line, pt2), 1.0));
-    EXPECT_TRUE(cmp::are_equal(distance(line, pt3), 1.0));
+    EXPECT_TRUE (cmp::are_equal (distance (line, pt1), 0.0));
+    EXPECT_TRUE (cmp::are_equal (distance (line, pt2), 1.0));
+    EXPECT_TRUE (cmp::are_equal (distance (line, pt3), 1.0));
 }
 
-TEST(Lines, distance_btw_lines)
+TEST (Lines, distance_btw_lines)
 {
     Point pt1 {0.0, 0.0, 0.0};
     Point pt2 {1.0, 1.0, 1.0};
@@ -78,18 +79,18 @@ TEST(Lines, distance_btw_lines)
 
     Line line1 {pt1, vec1};
     Line line2 {pt2, vec2};
-    EXPECT_TRUE(cmp::are_equal(distance(line1, line2), 1.0));
+    EXPECT_TRUE (cmp::are_equal (distance (line1, line2), 1.0));
 
     Line line3 {pt3, vec1};
-    EXPECT_TRUE(cmp::are_equal(distance(line1, line3), 1.0));
+    EXPECT_TRUE (cmp::are_equal (distance (line1, line3), 1.0));
 
     Line line4 {pt1, vec2};
     Line line5 {pt3, vec2};
-    EXPECT_TRUE(cmp::are_equal(distance(line4, line5), 1.0 / std::sqrt(2.0)));
-    
-    Line line6 {pt2, vec3};
-    EXPECT_TRUE(cmp::are_equal(distance(line1, line6), std::sqrt(2.0)));
+    EXPECT_TRUE (cmp::are_equal (distance (line4, line5), 1.0 / std::sqrt (2.0)));
 
-    EXPECT_TRUE(cmp::are_equal(distance(line1, line4), 0.0));
-    EXPECT_TRUE(cmp::are_equal(distance(line2, line6), 0.0));
+    Line line6 {pt2, vec3};
+    EXPECT_TRUE (cmp::are_equal (distance (line1, line6), std::sqrt (2.0)));
+
+    EXPECT_TRUE (cmp::are_equal (distance (line1, line4), 0.0));
+    EXPECT_TRUE (cmp::are_equal (distance (line2, line6), 0.0));
 }
