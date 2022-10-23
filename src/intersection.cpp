ef698c9b390b5597e2580da2d280e167ce5bd455
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-10-23 23:31:33 +0300

    Problem with testing space transformation is fixed

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -24,69 +24,7 @@ void point_transformation (Point &point, const Point &origin, const Vector &x_ax
 {
     auto diff_x = point.x_ - origin.x_;
     auto diff_y = point.y_ - origin.y_;
-    auto diff_z = point.z_ - origin.z_;  
-
-    point.x_ = x_axis.x_ * diff_x + x_axis.y_ * diff_y + x_axis.z_ * diff_z;
-    point.y_ = y_axis.x_ * diff_x + y_axis.y_ * diff_y + y_axis.z_ * diff_z;
-    point.z_ = 0.0;
-}
-
-void space_transformation (Triangle &tr_1, Triangle &tr_2)
-{
-    Vector x_axis {tr_1.P_, tr_1.Q_};
-    x_axis *= (1 / x_axis.module ());
-
-    Vector normal = vector_product (Vector {tr_1.P_, tr_1.Q_}, Vector {tr_1.P_, tr_1.R_});
-    normal *= (1 / normal.module ());
-
-    Vector y_axis = vector_product (normal, x_axis);
-
-    Point origin = tr_1.P_;
-
-    tr_1.P_ = {0.0, 0.0, 0.0}; //  this is origin
-    point_transformation (tr_1.Q_, origin, x_axis, y_axis);
-    point_transformation (tr_1.R_, origin, x_axis, y_axis);
-
-    point_transformation (tr_2.P_, origin, x_axis, y_axis);
-    point_transformation (tr_2.Q_, origin, x_axis, y_axis);
-    point_transformation (tr_2.R_, origin, x_axis, y_axis);
-}
-
-bool test_intersection_R1 (const Triangle &tr_1, const Triangle &tr_2)
-{
-    if (magic_product (tr_1.R_, tr_1.P_, tr_2.Q_) == Loc_2D::Negative)
-    {
-        return !(magic_product (tr_1.R_, tr_1.P_, tr_2.R_) == Loc_2D::Negative ||
-                 magic_product (tr_2.Q_, tr_2.R_, tr_1.R_) == Loc_2D::Negative ||
-                 magic_product (tr_2.P_, tr_1.P_, tr_2.R_) == Loc_2D::Positive);
-    }
-    else
-    {
-        if (magic_product (tr_1.R_, tr_2.P_, tr_2.Q_) == Loc_2D::Negative)
-            return false;
-        else if (magic_product (tr_2.P_, tr_1.P_, tr_2.Q_) != Loc_2D::Negative)
-            return true;
-        else if (magic_product (tr_2.P_, tr_1.P_, tr_2.R_) == Loc_2D::Negative ||
-                 magic_product (tr_2.R_, tr_2.R_, tr_1.P_) == Loc_2D::Negative)
-            return false;
-        else
-            return true;
-    }
-}
-
-/* 
- * Block with test of intersection in 2D
- *
- * Function with space_transformation
- * All coordinates of each point are transformed to 
- * coordinates in the mutual plane
- */
-void point_transformation (Point &point, const Point &origin, const Vector &x_axis,
-                           const Vector &y_axis)
-{
-    auto diff_x = point.x_ - origin.x_;
-    auto diff_y = point.y_ - origin.y_;
-    auto diff_z = point.z_ - origin.z_;  
+    auto diff_z = point.z_ - origin.z_;
 
     point.x_ = x_axis.x_ * diff_x + x_axis.y_ * diff_y + x_axis.z_ * diff_z;
     point.y_ = y_axis.x_ * diff_x + y_axis.y_ * diff_y + y_axis.z_ * diff_z;
--- test/include/algorithm_testing.hpp
+++ test/include/algorithm_testing.hpp
@@ -6,8 +6,9 @@
 namespace Geom_Objects
 {
 
-void transform_triangle (Triangle &tr_1, const Loc_3D P1_loc, const Loc_3D Q1_loc, const Loc_3D R1_loc, Triangle &tr_2);
-
+void space_transformation (Triangle &tr_1, Triangle &tr_2);
+void transform_triangle (Triangle &tr_1, const Loc_3D P1_loc, const Loc_3D Q1_loc,
+                         const Loc_3D R1_loc, Triangle &tr_2);
 }
 
 #endif // TEST_INCLUDE_ALGORITHM_TESTING_HPP
