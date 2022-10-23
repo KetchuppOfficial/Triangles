c3d54a2176b1ba56ba5cb7e5e553279b22b17b23
KetchuppOfficial <dolgov04@list.ru>
2022-10-24 00:58:31 +0300

    Test for magic product added

--- test/basic_tests/src/point_test.cpp
+++ test/basic_tests/src/point_test.cpp
@@ -69,3 +69,18 @@ TEST (Points, Validity)
 
     EXPECT_TRUE (Point {}.is_valid ());
 }
+
+TEST (Points, Magic_Product_3D)
+{
+    Point pt_11 {1.0, 0.0, 0.0};
+    Point pt_21 {0.0, 1.0, 0.0};
+    Point pt_31 {0.0, 0.0, 0.0};
+
+    EXPECT_EQ (magic_product (pt_11, pt_21, pt_31, {802.82, -872.85, 48.14}),  Loc_3D::Above);
+    EXPECT_EQ (magic_product (pt_11, pt_21, pt_31, {802.82, -872.85, 0.0}),    Loc_3D::On);
+    EXPECT_EQ (magic_product (pt_11, pt_21, pt_31, {802.82, -872.85, -2.865}), Loc_3D::Below);
+
+    EXPECT_EQ (magic_product (pt_11, pt_31, pt_21, {802.82, -872.85, 48.14}),  Loc_3D::Below);
+    EXPECT_EQ (magic_product (pt_11, pt_31, pt_21, {802.82, -872.85, 0.0}),    Loc_3D::On);
+    EXPECT_EQ (magic_product (pt_11, pt_31, pt_21, {802.82, -872.85, -2.865}), Loc_3D::Above);
+}
