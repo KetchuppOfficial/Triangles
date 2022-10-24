327cb2290a60941a7f8b4a6d24b2534b4c7c496b
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-10-25 00:01:38 +0300

    Declarations of some functions were placed here for testing them

--- test/include/algorithm_testing.hpp
+++ test/include/algorithm_testing.hpp
@@ -6,6 +6,8 @@
 namespace Geom_Objects
 {
 
+bool intersection_in_2D (const Triangle &tr_1_, const Triangle &tr_2_);
+inline Loc_2D magic_product (const Point &P, const Point &Q, const Point &M);
 void space_transformation (Triangle &tr_1, Triangle &tr_2);
 void transform_triangle (Triangle &tr_1, const Loc_3D P1_loc, const Loc_3D Q1_loc,
                          const Loc_3D R1_loc, Triangle &tr_2);
