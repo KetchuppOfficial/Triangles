f3b5c9a7c04db5b2f25e32208d000255fe867a3c
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-10-25 00:06:21 +0300

    Ordering of point in testing functions for case R1 and R2 were changed to opposite

--- include/intersection.hpp
+++ include/intersection.hpp
@@ -8,15 +8,6 @@
 namespace Geom_Objects
 {
 
-bool intersection_in_2D (const Triangle &tr_1, const Triangle &tr_2);
-void point_transformation (Point &point, const Point &origin, const Vector &x_axis,
-                           const Vector &y_axis);
-void space_transformation (Triangle &tr_1, Triangle &tr_2);
-bool test_intersection_R1 (const Triangle &tr_1, const Triangle &tr_2);
-bool test_intersection_R2 (const Triangle &tr_1, const Triangle &tr_2);
-
-
-
 bool are_intersecting (Triangle &tr_1, Triangle &tr_2);
 
 } // namespace Geom_Objects
