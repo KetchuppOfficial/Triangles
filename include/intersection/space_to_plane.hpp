b865fce52f8f1e543f378ddeccba0ee79a8206ef
KetchuppOfficial <dolgov04@list.ru>
2023-02-26 15:05:28 +0300

    Transition to 2D coodinates was replaced by orthogonal projection

--- include/intersection/space_to_plane.hpp
+++ include/intersection/space_to_plane.hpp
@@ -2,7 +2,9 @@
 #define INCLUDE_INTERSECION_SPACE_TO_PLANE_HPP
 
 #include <utility>
-#include <tuple>
+#include <algorithm>
+#include <cassert>
+#include <exception>
 
 #include "point.hpp"
 #include "segment.hpp"
@@ -15,99 +17,124 @@ namespace yLab::geometry
 namespace detail
 {
 
+enum class Axises { x, y, z };
+
 template<typename T>
-Point_2D<T> point_transformation (const Point_3D<T> &point, const Point_3D<T> &origin, 
-                                  const Vector<T> &x_axis,  const Vector<T> &y_axis)
+Axises deduce_coordinate_to_set_zero (const Vector<T> &vec_1, const Vector<T> &vec_2)
 {
-    auto diff_x = point.x_ - origin.x_;
-    auto diff_y = point.y_ - origin.y_;
-    auto diff_z = point.z_ - origin.z_;
-
-    Point_2D<T> pt{x_axis.x_ * diff_x + x_axis.y_ * diff_y + x_axis.z_ * diff_z,
-                   y_axis.x_ * diff_x + y_axis.y_ * diff_y + y_axis.z_ * diff_z};
-
-    return pt;
-}
-
-template<typename T>
-auto get_2d_coordinates (const Triangle<Point_3D<T>> &tr)
-{    
-    Vector x_axis{tr.P(), tr.Q()};
-    Vector z_axis = vector_product (x_axis, Vector{tr.P(), tr.R()});
-    Vector y_axis = vector_product (z_axis, x_axis);
-
-    x_axis.normalize();
-    y_axis.normalize();
-
-    return std::tuple<Point_3D<T>, Vector<T>, Vector<T>>{tr.P(), x_axis, y_axis};
-}
-
-template<typename T>
-auto space_transformation (const Triangle<Point_3D<T>> &tr_1, const Triangle<Point_3D<T>> &tr_2)
-{
-    using triangle_2d = Triangle<Point_2D<T>>;
-    using triangle_pair = std::pair<triangle_2d, triangle_2d>;
+    assert (are_collinear (vec_1, vec_2) == false);
     
-    auto [origin, x_axis, y_axis] = get_2d_coordinates (tr_1);
+    Vector norm = vector_product(vec_1, vec_2);
+    auto x = std::abs (norm.x_);
+    auto y = std::abs (norm.y_);
+    auto z = std::abs (norm.z_);
 
-    triangle_2d new_tr_1 {Point_2D<T>{},
-                          point_transformation (tr_1.Q(), origin, x_axis, y_axis),
-                          point_transformation (tr_1.R(), origin, x_axis, y_axis)};
-
-    triangle_2d new_tr_2 {point_transformation (tr_2.P(), origin, x_axis, y_axis),
-                          point_transformation (tr_2.Q(), origin, x_axis, y_axis),
-                          point_transformation (tr_2.R(), origin, x_axis, y_axis)};
-
-    return triangle_pair{new_tr_1, new_tr_2};
+    if (x > y && x > z)
+        return Axises::x;
+    else if (y > z)
+        return Axises::y;
+    else
+        return Axises::z;
 }
 
 template<typename T>
-auto space_transformation (const Triangle<Point_3D<T>> &tr, const Point_3D<T> &pt)
+Point_2D<T> project_point (const Point_3D<T> &pt, Axises coordinate_to_set_zero)
 {
-    using point_2d = Point_2D<T>;
-    using triangle_2d = Triangle<point_2d>;
-    using point_triangle_pair = std::pair<point_2d, triangle_2d>;
-    
-    auto [origin, x_axis, y_axis] = get_2d_coordinates (tr);
+    switch (coordinate_to_set_zero)
+    {
+        case Axises::x:
+            return Point_2D{pt.y_, pt.z_};
 
-    point_2d new_pt = point_transformation (pt, origin, x_axis, y_axis);
+        case Axises::y:
+            return Point_2D{pt.z_, pt.x_};
 
-    triangle_2d new_tr {Point_2D<T>{},
-                        point_transformation (tr.Q(), origin, x_axis, y_axis),
-                        point_transformation (tr.R(), origin, x_axis, y_axis)};
+        case Axises::z:
+            return Point_2D{pt.x_, pt.y_};
 
-    return point_triangle_pair{new_pt, new_tr};
+        default:
+            throw std::runtime_error ("Unexpected value of coordinate_to_set_zero");
+    }
+}
+
+template<typename T>
+Segment<Point_2D<T>> project_segment (const Segment<Point_3D<T>> &seg,
+                                      Axises coordinate_to_set_zero)
+{
+    switch (coordinate_to_set_zero)
+    {
+        case Axises::x:
+        {
+            return Segment{Point_2D{seg.P().y_, seg.P().z_}, 
+                           Point_2D{seg.Q().y_, seg.Q().z_}};
+        }
+
+        case Axises::y:
+        {
+            return Segment{Point_2D{seg.P().z_, seg.P().x_}, 
+                           Point_2D{seg.Q().z_, seg.Q().x_}};
+        }
+
+        case Axises::z:
+        {
+            return Segment{Point_2D{seg.P().x_, seg.P().y_}, 
+                           Point_2D{seg.Q().x_, seg.Q().y_}};
+        }
+
+        default:
+            throw std::runtime_error ("Unexpected value of coordinate_to_set_zero");
+    }
+}
+
+template<typename T>
+Triangle<Point_2D<T>> project_triangle (const Triangle<Point_3D<T>> &tr,
+                                        Axises coordinate_to_set_zero)
+{
+    switch (coordinate_to_set_zero)
+    {
+        case Axises::x:
+        {
+            return Triangle{Point_2D{tr.P().y_, tr.P().z_}, 
+                            Point_2D{tr.Q().y_, tr.Q().z_},
+                            Point_2D{tr.R().y_, tr.R().z_}};
+        }
+
+        case Axises::y:
+        {
+            return Triangle{Point_2D{tr.P().z_, tr.P().x_}, 
+                            Point_2D{tr.Q().z_, tr.Q().x_},
+                            Point_2D{tr.R().z_, tr.R().x_}};
+        }
+
+        case Axises::z:
+        {
+            return Triangle{Point_2D{tr.P().x_, tr.P().y_}, 
+                            Point_2D{tr.Q().x_, tr.Q().y_},
+                            Point_2D{tr.R().x_, tr.R().y_}};
+        }
+
+        default:
+            throw std::runtime_error ("Unexpected value of coordinate_to_set_zero");
+    }
 }
 
 template<typename T>
 auto space_transformation (const Point_3D<T> &pt, const Triangle<Point_3D<T>> &tr)
 {
-    return space_transformation (tr, pt);
-}
-
-template<typename T>
-auto space_transformation (const Triangle<Point_3D<T>> &tr, const Segment<Point_3D<T>> &seg)
-{
-    using segment_2d = Segment<Point_2D<T>>;
-    using triangle_2d = Triangle<Point_2D<T>>;
-    using segment_triangle_pair = std::pair<segment_2d, triangle_2d>;
+    using point_2d = Point_2D<T>;
+    using triangle_2d = Triangle<point_2d>;
+    using point_triangle_pair = std::pair<point_2d, triangle_2d>;
     
-    auto [origin, x_axis, y_axis] = get_2d_coordinates (tr);
+    auto coordinate_to_set_zero = deduce_coordinate_to_set_zero (Vector{tr.P(), tr.Q()},
+                                                                 Vector{tr.P(), tr.R()});
 
-    segment_2d new_seg {point_transformation (seg.P(), origin, x_axis, y_axis),
-                        point_transformation (seg.Q(), origin, x_axis, y_axis)};
-
-    triangle_2d new_tr {Point_2D<T>{},
-                        point_transformation (tr.Q(), origin, x_axis, y_axis),
-                        point_transformation (tr.R(), origin, x_axis, y_axis)};
-
-    return segment_triangle_pair{new_seg, new_tr};
+    return point_triangle_pair{project_point (pt, coordinate_to_set_zero),
+                               project_triangle (tr, coordinate_to_set_zero)};
 }
 
 template<typename T>
-auto space_transformation (const Segment<Point_3D<T>> &seg, const Triangle<Point_3D<T>> &tr)
+auto space_transformation (const Triangle<Point_3D<T>> &tr, const Point_3D<T> &pt)
 {
-    return space_transformation (tr, seg);
+    return space_transformation (pt, tr);
 }
 
 template<typename T>
@@ -116,23 +143,44 @@ auto space_transformation (const Segment<Point_3D<T>> &seg_1, const Segment<Poin
     using segment_2d = Segment<Point_2D<T>>;
     using segment_pair = std::pair<segment_2d, segment_2d>;
 
-    // Think how to substitute following code with get_2d_axes()
-    // =========================================================
-    Vector<T> x_axis {seg_1.P(), seg_1.Q()};
-    Vector<T> z_axis = vector_product (x_axis, Vector<T>{seg_2.P(), seg_2.Q()});
-    Vector<T> y_axis = vector_product (z_axis, x_axis);
+    auto coordinate_to_set_zero = deduce_coordinate_to_set_zero (Vector{seg_1.P(), seg_1.Q()},
+                                                                 Vector{seg_2.P(), seg_2.Q()});
 
-    x_axis.normalize();
-    y_axis.normalize();
-    // =========================================================
+    return segment_pair{project_segment (seg_1, coordinate_to_set_zero),
+                        project_segment (seg_2, coordinate_to_set_zero)};
+}
 
-    segment_2d new_seg_1 {Point_2D<T>{}, 
-                          point_transformation (seg_1.Q(), seg_1.P(), x_axis, y_axis)};
+template<typename T>
+auto space_transformation (const Segment<Point_3D<T>> &seg, const Triangle<Point_3D<T>> &tr)
+{
+    using segment_2d = Segment<Point_2D<T>>;
+    using triangle_2d = Triangle<Point_2D<T>>;
+    using segment_triangle_pair = std::pair<segment_2d, triangle_2d>;
 
-    segment_2d new_seg_2 {point_transformation (seg_2.P(), seg_1.P(), x_axis, y_axis),
-                          point_transformation (seg_2.Q(), seg_1.P(), x_axis, y_axis)};
+    auto coordinate_to_set_zero = deduce_coordinate_to_set_zero (Vector{tr.P(), tr.Q()},
+                                                                 Vector{tr.P(), tr.R()});
 
-    return segment_pair{new_seg_1, new_seg_2};
+    return segment_triangle_pair{project_segment (seg, coordinate_to_set_zero),
+                                 project_triangle (tr, coordinate_to_set_zero)};
+}
+
+template<typename T>
+auto space_transformation (const Triangle<Point_3D<T>> &tr, const Segment<Point_3D<T>> &seg)
+{
+    return space_transformation (seg, tr);
+}
+
+template<typename T>
+auto space_transformation (const Triangle<Point_3D<T>> &tr_1, const Triangle<Point_3D<T>> &tr_2)
+{
+    using triangle_2d = Triangle<Point_2D<T>>;
+    using triangle_pair = std::pair<triangle_2d, triangle_2d>;
+
+    auto coordinate_to_set_zero = deduce_coordinate_to_set_zero (Vector{tr_1.P(), tr_1.Q()},
+                                                                 Vector{tr_1.P(), tr_1.R()});
+
+    return triangle_pair{project_triangle (tr_1, coordinate_to_set_zero),
+                         project_triangle (tr_2, coordinate_to_set_zero)};
 }
 
 } // namespace detail
