5569501362192fd5b4cfce3b5d16041287b3d6f3
KetchuppOfficial <dolgov04@list.ru>
2023-03-09 23:34:17 +0300

    Driver was revised due to changes in the system of classes of the project and implementation of octree

--- test/end_to_end/driver.cpp
+++ test/end_to_end/driver.cpp
@@ -1,115 +1,99 @@
+// clang++ -std=c++20 -I../../include -I../../include/space_partitioning -I../../include/primitives/ -I../../include/intersection driver.cpp
+
 #include <iostream>
 #include <iterator>
 #include <variant>
 #include <vector>
 
-#include "point_point.hpp"
-#include "point_segment.hpp"
-#include "point_triangle.hpp"
-#include "segment_segment.hpp"
-#include "segment_triangle.hpp"
-#include "triangle_triangle.hpp"
+#include "intersector.hpp"
 
-using point = yLab::geometry::Point_3D<float>;
 using vector = yLab::geometry::Vector<float>;
-using segment = yLab::geometry::Segment<point>;
-using triangle = yLab::geometry::Triangle<point>;
-using shape = std::variant<point, segment, triangle>;
+
+using point_type    = yLab::geometry::Primitive_Traits<float>::point_type;
+using segment_type  = yLab::geometry::Primitive_Traits<float>::segment_type;
+using triangle_type = yLab::geometry::Primitive_Traits<float>::triangle_type;
+using shape = yLab::geometry::Shape<float>;
+
+using intersector = yLab::geometry::Intersector<float>;
 
 namespace
 {
 
-std::vector<point> construct_points ()
+std::vector<point_type> construct_points ()
 {
     auto cin_iter = std::istream_iterator<int>{std::cin};
     auto cin_end  = std::istream_iterator<int>{};
-    auto n_points = *cin_iter * 3;
+    auto n_points = *cin_iter;
     cin_iter++;
     
-    std::vector<point> points;
+    std::vector<point_type> points;
     for (auto i = 0; i != n_points && cin_iter != cin_end; ++i)
     {
         float x = *cin_iter++;
         float y = *cin_iter++;
         float z = *cin_iter++;
         
-        points.push_back (point{x, y, z});
+        points.push_back (point_type{x, y, z});
     }
 
     return points;
 }
 
-segment assemble_segment (const point &P, const point &Q, const point &R)
+segment_type assemble_segment (const point_type &P, const point_type &Q, const point_type &R)
 {
     vector PR{P, R};
     vector QR{Q, R};
 
     if (are_oppositely_directed (PR, QR))
-        return segment{P, Q};
+        return segment_type{P, Q};
     else if (PR.norm() > QR.norm())
-        return segment{P, R};
+        return segment_type{P, R};   
     else
-        return segment{Q, R};
+        return segment_type{Q, R};
 }
 
-std::vector<shape> construct_triangles (const std::vector<point> &points)
+template<std::input_iterator it>
+std::vector<shape> construct_shapes (it first, it last)
 {
-    auto n_triangles = points.size() / 3;
     std::vector<shape> triangles;
+    triangles.reserve (std::distance (first, last) / 3);
 
-    for (auto i = 0; i != n_triangles;)
+    while (first != last)
     {
-        auto P = points[i++];
-        auto Q = points[i++];
-        auto R = points[i++];
+        auto &P = *first++;
+        auto &Q = *first++;
+        auto &R = *first++;
         
         try
         {
-            shape figure{triangle{P, Q, R}};
-            triangles.push_back (std::get<triangle>(figure));
-        }
-        catch (yLab::geometry::Triangle_Is_Point &expt)
-        {
-            shape figure{P};
-            triangles.push_back (std::get<point>(figure));
+            triangle_type tr{P, Q, R};
+            triangles.emplace_back (tr);
         }
         catch (yLab::geometry::Triangle_Is_Segment &expt)
         {
-            shape figure = assemble_segment (P, Q, R);
-            triangles.push_back (std::get<segment>(figure));
+            segment_type seg = assemble_segment (P, Q, R);
+            triangles.emplace_back (seg);
+        }
+        catch (yLab::geometry::Triangle_Is_Point &expt)
+        {
+            triangles.emplace_back (P);
         }
     }
 
     return triangles;
 }
 
-void intersect_extremely_slow (const std::vector<shape> &triangles)
-{
-    auto n_triangles = triangles.size();
-    for (auto i = 0; i != n_triangles; ++i)
-    {
-        for (auto j = i + 1; j != n_triangles; ++j)
-        {
-            auto intersection = std::visit ([](auto &shape_1, auto &shape_2)
-                                            {
-                                                return are_intersecting (shape_1, shape_2);
-                                            },
-                                            triangles[i], triangles[j]);
-
-            if (intersection)
-                std::cout << i << " " << j << std::endl;
-        }
-    }
-}
-
 } // unnamed namespace
+
+#define OCTREE_RULES
 
 int main ()
 {
-    std::vector<point> points    = construct_points ();
-    std::vector<shape> triangles = construct_triangles (points);
+    std::vector<point_type> points = construct_points ();
+    std::vector<shape> shapes = construct_shapes (points.begin(), points.end());
 
-    intersect_extremely_slow (triangles);
+    intersector collider {shapes.begin(), shapes.end()};
+    collider.intersect_all();
 
     return 0;
 }
