b08c40c17e726020ecfb3ba4a0209ad7e485eb97
KetchuppOfficial <dolgov04@list.ru>
2023-03-09 23:27:51 +0300

    Octree was implemented

---
+++ include/space_partitioning/node.hpp
@@ -0,0 +1,94 @@
+#ifndef INCLUDE_SPACE_PARTITIONING_NODE_HPP
+#define INCLUDE_SPACE_PARTITIONING_NODE_HPP
+
+#include <cmath>
+#include <array>
+#include <vector>
+#include <algorithm>
+
+#include "vector"
+#include "shape.hpp"
+#include "primitive_traits.hpp"
+
+namespace yLab
+{
+
+namespace geometry
+{
+
+template<typename T>
+class Octree_Node final
+{
+public:
+
+    using distance_type = T;
+    using vector_type = Vector<distance_type>;
+    using point_type = typename Primitive_Traits<distance_type>::point_type;
+    using shape_type = Shape<distance_type>;
+    
+private:
+
+    std::array<Octree_Node *, 8> children_{};
+    std::vector<shape_type> shapes_;
+    point_type center_;
+    distance_type halfwidth_;
+
+public:
+
+    Octree_Node (const point_type &center, distance_type halfwidth)
+                : center_{center}, halfwidth_{halfwidth} {}
+
+    // Accessors
+
+    const Octree_Node *child (unsigned child_i) const { return children_[child_i]; }
+    Octree_Node *&child (unsigned child_i) { return children_[child_i]; }
+
+    const point_type &center () const { return center_; }
+    distance_type halfwidth () const { return halfwidth_; }
+
+    const std::vector<shape_type> &shapes () const { return shapes_; }
+    std::vector<shape_type> &shapes () { return shapes_; }
+
+    // Modifiers
+
+    void add_shape (const shape_type &shape) { shapes_.push_back (shape); }
+};
+
+namespace detail
+{
+
+template<typename T>
+void insert_shape (Octree_Node<T> *root, const typename Octree_Node<T>::shape_type &shape)
+{
+    auto index = 0;
+    bool within_range = false;
+
+    auto &bounding_volume = shape.bounding_volume();
+
+    for (auto i = 0; i != 3; ++i)
+    {
+        auto delta = bounding_volume.center()[i] - root->center()[i];
+
+        if (std::abs (delta) < root->halfwidth() + bounding_volume.halfwidth (i))
+        {
+            within_range = true;
+            break;
+        }
+        
+        if (delta > T{})
+            index |= (1 << i);
+    }
+
+    if (within_range == false && root->child (index) != nullptr)
+        insert_shape (root->child (index), shape);
+    else
+        root->add_shape (shape);
+}
+
+} // namespace detail
+
+} // namespace geometry
+
+} // namespace yLab
+
+#endif // INCLUDE_SPACE_PARTITIONING_NODE_HPP
---
+++ include/space_partitioning/octree.hpp
@@ -0,0 +1,153 @@
+#ifndef INCLUDE_SPACE_PARTITIONING_OCTREE_HPP
+#define INCLUDE_SPACE_PARTITIONING_OCTREE_HPP
+
+#include <vector>
+#include <cmath>
+#include <exception>
+#include <iterator>
+#include <utility>
+
+#include "shape.hpp"
+#include "node.hpp"
+#include "primitive_traits.hpp"
+
+namespace yLab
+{
+
+namespace geometry
+{
+
+namespace detail
+{
+
+template<std::input_iterator it>
+auto calculate_octree_parameters (it first, it last)
+{
+    using distance_type = it::value_type::distance_type;
+    
+    std::array<distance_type, 3> min{};
+    std::array<distance_type, 3> max{};
+    
+    for (; first != last; ++first)
+    {
+        auto &bounding_volume = (*first).bounding_volume();
+        auto &center = bounding_volume.center();
+
+        for (auto i = 0; i != 3; ++i)
+        {
+            auto left_border_i = center[i] - bounding_volume.halfwidth(i);
+            auto right_border_i = center[i] + bounding_volume.halfwidth(i);
+
+            if (left_border_i < min[i])
+                min[i] = left_border_i;
+
+            if (right_border_i > max[i])
+                max[i] = right_border_i;
+        }
+    }
+
+    auto min_elem = std::min_element (min.begin(), min.end());
+    auto max_elem = std::max_element (max.begin(), max.end());
+
+    auto pt_coord = (*min_elem + *max_elem) / distance_type{2};
+    auto halfwidht = (*max_elem - *min_elem) / distance_type{2};
+
+    return std::pair{Point_3D{pt_coord, pt_coord, pt_coord}, halfwidht};
+}
+
+} // namespace detail
+
+struct Empty_Octree final: public std::runtime_error
+{
+    Empty_Octree () : std::runtime_error{"Constructing octree of height 0 is forbidden"} {}
+};
+
+template<typename T>
+class Octree final
+{
+public:
+
+    using distance_type = T;
+    using point_type = typename Primitive_Traits<T>::point_type;
+    using shape_type = Shape<distance_type>;
+    using node_type = Octree_Node<distance_type>;
+    using size_type = std::size_t;
+
+private:
+
+    std::vector<node_type> nodes_;
+    size_type height_ = 0;
+
+public:
+    
+    template<std::input_iterator it>
+    Octree (unsigned height, it first, it last) : height_{height}
+    {
+        if (height_ == 0)
+            throw Empty_Octree{};
+
+        nodes_.reserve (max_size());
+
+        auto [center, halfwidth] = detail::calculate_octree_parameters (first, last);
+        build_subtree (center, halfwidth, height_);
+
+        insert (first, last);
+    }
+
+    size_type height () const { return height_; }
+    size_type size () const { return nodes_.size(); }
+    size_type max_size () const
+    {
+        return (static_cast<size_type>(std::pow (8, height())) - 1) / (8 - 1);
+    }
+
+    // Modifiers
+
+    void insert (const shape_type &shape)
+    {
+        detail::insert_shape (std::addressof (root()), shape);
+    }
+
+    template<std::input_iterator it>
+    requires std::same_as<std::iter_value_t<it>, shape_type>
+    void insert (it first, it last)
+    {
+        for (; first != last; ++first)
+            insert (*first);
+    }
+
+    const node_type &root () const { return nodes_.front(); }
+    node_type &root () { return nodes_.front(); }
+
+private:
+
+    // At least max_size() bytes has to be preallocated
+    node_type *build_subtree (const point_type &center, distance_type halfwidth, unsigned stop_depth)
+    {
+        node_type &subroot = nodes_.emplace_back(center, halfwidth);
+        
+        if (stop_depth > 1)
+        {
+            auto step = halfwidth * distance_type{0.5};
+            for (int child_i = 0; child_i != 8; ++child_i)
+            {
+                point_type offset {(child_i & 1) ? step : -step,
+                                   (child_i & 2) ? step : -step,
+                                   (child_i & 4) ? step : -step};
+
+                point_type new_center{center.x() + offset.x(), center.y() + offset.y(),
+                                      center.z() + offset.z()};
+
+                subroot.child(child_i) = build_subtree (new_center, step, stop_depth - 1);
+            }
+        }
+
+        return std::addressof (subroot);
+    }
+};
+
+} // namespace geometry
+
+} // namespace yLab
+
+#endif // INCLUDE_SPACE_PARTITIONING_OCTREE_HPP
